3ddbd2a4448d0ebcf50c0a8ebfd605b6d9ab8f0f
Yasmin  Nadia <141208520+yasmin-nadia@users.noreply.github.com>
2023-10-30 14:01:11 +0600

    Update axiosInstance.js

--- react-introduction/src/utils/axiosInstance.js
+++ react-introduction/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 console.log("UseDeleteHook token", token);
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/mybooks",
+  baseURL: "http://13.38.117.51:8000/mybooks",
   headers: {
     "Content-Type": "application/json",
 
